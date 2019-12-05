964cf6606efed03c60f560dbff0d68df083adece
Jfs <jf.stassen@gmail.com>
2019-12-05 14:55:39 +0100

    finish app

---
+++ src/DogDetails.js
@@ -0,0 +1,43 @@
+import React, { Component } from "react";
+import uuid from "uuid/v4";
+import {Link} from "react-router-dom"
+
+export default class DogDetails extends Component {
+    render() {
+        let { dog } = this.props;
+        return (
+
+                <div className="DogDetails row justify-content-center mt-5">
+                    <div className="col-11 col-lg-5">
+                        <div className="DogDetails-card card">
+                            <img
+                                className="card-img-top"
+                                src={dog.src}
+                                alt={dog.name}
+                            />
+                            <div className="card-body">
+                                <h2 className="card-title">{dog.name}</h2>
+                                <h4 className="card-subtitle text-muted">
+                                    {dog.age} years old
+                                </h4>
+                            </div>
+                            <ul className="list-group list-group-flush">
+                                {dog.facts.map(f => (
+                                    <li
+                                        key={uuid()}
+                                        className="list-group-item"
+                                    >
+                                        {f}
+                                    </li>
+                                ))}
+                            </ul>
+                            <div className="card-body">
+                              <Link className="btn btn-info" to="/dogs">Go back</Link>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+        );
+    }
+}
